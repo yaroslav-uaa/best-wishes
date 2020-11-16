2c5e39ba6bc036a7afb6ab27031331230af6a869
Yaroslav <ypakhachuk@gmail.com>
2020-11-16 10:55:32 +0200

    fix all

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let a = prompt("Enter UserName");
+let b = prompt("Enter password");
+if (a == "Marinessa" && b == "15111994") {
+  window.location.href = "bday.html";
+} else {
+  alert("invalid details");
+  window.location.href = "index.html";
+}
